285139fa6be129238ffaf96122fadd32f4c92ed4
dzhav <fataliev.ds@phystech.edu>
2022-11-15 13:10:47 +0300

    #5 commit

--- 1.cpp
+++ 1.cpp
@@ -2,9 +2,14 @@
 using namespace std;
 
 int main(){
-int arr[3]={ 4, 5, 10}
+int n;
+cin>>n;
+int arr[100];
+for ( int i =0; i < n; i++){
+cin>>arr[i];
+}
 int numi = -1999999;
-for (int i = 0; i < 3; i++)
+for (int i = 0; i < n; i++)
 numi=max(numi, arr[i]);
 cout<<numi;
 }
